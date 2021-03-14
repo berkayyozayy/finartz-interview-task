545894d325aad4128518d3635dd335f4a5069250
berkayozay <berkayyozayy@gmail.com>
2021-03-14 13:52:08 +0300

    url bug fix

--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -4,13 +4,13 @@ import HeaderColumnTwo from './HeaderColumnTwo';
 import CardsView from '../container/CardsView';
 
 const URLs = {
-    'films': 'https://api.themoviedb.org/3/movie/upcoming?api_key=267478f9e81983e4ecdb72c1b7954e41&language=en-US&page=1',
+    'films': 'https://api.themoviedb.org/3/movie/now_playing?api_key=267478f9e81983e4ecdb72c1b7954e41&language=en-US&page=3',
     'onTv': 'https://api.themoviedb.org/3/tv/on_the_air?api_key=267478f9e81983e4ecdb72c1b7954e41&language=en-US&page=1',
 }
 
 const SectionTwo = () => {
 
-    const [url, setUrl] = useState('https://api.themoviedb.org/3/movie/now_playing?api_key=267478f9e81983e4ecdb72c1b7954e41&language=en-US&page=1')
+    const [url, setUrl] = useState('https://api.themoviedb.org/3/movie/now_playing?api_key=267478f9e81983e4ecdb72c1b7954e41&language=en-US&page=3')
     const handleClick = (text) => {
         setUrl(URLs[text])
     }
